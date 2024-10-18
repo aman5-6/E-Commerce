39f2278c10acf1a9a428845905a78370892d8b65
Aman <165497962+aman5-6@users.noreply.github.com>
2024-10-18 19:39:28 +0530

    Add files via upload

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,44 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const { default: mongoose } = require("mongoose");
+const errorHandler = require("./middlewares/ErrorHandler");
+const InventoryRoutes = require("./routes/InventoryRoutes");
+const UserRoutes = require("./routes/UserRoutes");
+const AddressRoutes = require("./routes/AddressRoutes");
+const OrderRoutes = require("./routes/OrderRoutes");
+const Auth = require("./middlewares/Auth");
 
+// App Initialized
+const app = express();
+
+// Initial Middleware
+app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+app.use(morgan("tiny"));
+
+// Routes Calling
+app.use("/inventory", InventoryRoutes);
+app.use("/address", Auth, AddressRoutes);
+app.use("/order", Auth, OrderRoutes);
+app.use("/user", UserRoutes);
+
+// Final Middleware
+
+app.use(errorHandler); //Error Handling
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`Server is running on PORT ${PORT}`);
+  mongoose
+    .connect(
+      "mongodb://127.0.0.1:27017/mongopractice"
+    )
+    .then(() => console.log("DB Connected"))
+    .catch((e) => console.log("Error ==>", e.message));
+});
---
+++ Backend/models/AddressModel.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const addressSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users2",
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  streetName: {
+    type: String,
+    required: true,
+  },
+  houseNumber: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  pin: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    enum: ["Home", "Office", "Outdoor"],
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+  deletedAt: {
+    type: Date,
+  },
+});
+
+module.exports = mongoose.model("addresses", addressSchema);
